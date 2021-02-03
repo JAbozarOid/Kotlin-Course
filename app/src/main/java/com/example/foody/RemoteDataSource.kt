906ef62f75d6d0bedda5555fb6750421abd7ccf6
JAbozarOid <araghibdoust@hotmail.com>
2021-02-03 13:19:01 +0330

    remote_data_source_class

---
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+/**
+ * - this class wil basically request the data from our API,
+ * and before that, we're going to inject this for the Recipes API service(interface) inside our remote data source
+ * - we can actually request some new data from this remote data source class
+ */
+class RemoteDataSource() {
+}
--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -8,7 +8,19 @@ import android.view.ViewGroup
 import com.example.foody.R
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
-
+/**
+ * - RecipesFragment Class depend to MainViewModel class
+ * - MainViewModel class depend to Repository class
+ * - Repository class depend in two classes, one LocalDataSource(Room) class and second one RemoteDataSurce(Retrofit)
+ * - there is two different cases in which we are going to use a remote data source
+ * 1- Database is empty
+ * 2- Explicitly Request new data from our particular API
+ * - our application will always request the data from our local database and display that in recyclerview and only what we need
+ * - we are going to request the data from our remote data source,
+ * that way we are going to save a lot of bandwidth because we're not going to request data all the time from our API
+ * - Instead, we are going to use the offline cache to store new data from our API to our local database, and then after that, always retrieve that data from a local database,
+ * and only when we need we're going to request a new data from our API
+ */
 class RecipesFragment : Fragment() {
 
 
