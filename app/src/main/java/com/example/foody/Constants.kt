2f6c8c661c57dfb09ad10435a1916da07d3c3864
JAbozarOid <araghibdoust@hotmail.com>
2021-02-03 12:50:37 +0330

    recipe_api_interface

--- app/src/main/java/com/example/foody/Constants.kt
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -3,7 +3,7 @@ package com.example.foody
 class Constants {
 
     companion object {
-        const val API_KEY = ""
-        const val BASE_URL = ""
+        const val API_KEY = "https://api.spoonacular.com"
+        const val BASE_URL = "68a4b0c964ba407591ed2f724aadbe66"
     }
 }
---
+++ app/src/main/java/com/example/foody/FoodRecipeApi.kt
@@ -0,0 +1,17 @@
+package com.example.foody
+
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipeApi {
+
+    @GET("/recipes/complexSearch")
+    // this function will return FoodRecipe model
+    // we use suspend keyword, because our function will use kotlin coroutine and this function will run on a background thread instead of a main thread, so that's a huge bonus
+    suspend fun getRecipes(
+        // @QueryMap retrofit annotation will let us create something like a hash map to add all our queries at once
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+}
