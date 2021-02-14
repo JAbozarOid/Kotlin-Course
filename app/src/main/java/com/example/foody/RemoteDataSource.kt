1babd00519d6a938be452253db27fc74783b564b
JAbozarOid <araghibdoust@hotmail.com>
2021-02-14 17:04:20 +0330

    inject_remote_data_source

--- app/src/main/java/com/example/foody/RemoteDataSource.kt
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -1,9 +1,20 @@
 package com.example.foody
 
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import java.time.temporal.TemporalQueries
+import javax.inject.Inject
+
 /**
  * - this class wil basically request the data from our API,
  * and before that, we're going to inject this for the Recipes API service(interface) inside our remote data source
  * - we can actually request some new data from this remote data source class
  */
-class RemoteDataSource() {
+class RemoteDataSource @Inject constructor(
+    private val foodRecipeApi: FoodRecipeApi
+) {
+    // we use suspend keyword because getRecipes function is a suspend function
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipeApi.getRecipes(queries)
+    }
 }
