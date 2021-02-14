af81fb6794710422b8a438d0799be6e19ad21efc
JAbozarOid <araghibdoust@hotmail.com>
2021-02-14 16:19:17 +0330

    create_network_module

--- app/src/main/java/com/example/foody/Constants.kt
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -3,7 +3,7 @@ package com.example.foody
 class Constants {
 
     companion object {
-        const val API_KEY = "https://api.spoonacular.com"
-        const val BASE_URL = "68a4b0c964ba407591ed2f724aadbe66"
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "68a4b0c964ba407591ed2f724aadbe66"
     }
 }
