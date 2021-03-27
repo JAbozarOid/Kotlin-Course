9b381655ff04f91c1c53c6baa3c7402bf686f600
JAbozarOid <araghibdoust@hotmail.com>
2021-03-27 15:52:26 +0430

    create_database_class

---
+++ app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
@@ -0,0 +1,21 @@
+package com.example.foody.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+/**
+ * exportSchema means to export the database schema into a folder, it's not mandatory
+ * it's a good practice to have a history of your schema in your code base and you should commit the schema files into your control system
+ * when it's set to false it is disabled
+ */
+@Database(
+    entities = [RecipesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase : RoomDatabase() {
+
+    abstract fun recipesDao() : RecipesDao
+}
