ca2a338b1f4379a83bbdd0f742cc84289faf2c32
JAbozarOid <araghibdoust@hotmail.com>
2021-04-19 16:47:58 +0430

    create_search_api

--- app/src/main/java/com/example/foody/data/network/FoodRecipeApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipeApi.kt
@@ -12,6 +12,13 @@ interface FoodRecipeApi {
     // we use suspend keyword, because our function will use kotlin coroutine and this function will run on a background thread instead of a main thread, so that's a huge bonus
     suspend fun getRecipes(
         // @QueryMap retrofit annotation will let us create something like a hash map to add all our queries at once
+        // @QueryMap let us to pass multiple queries inside our get request
         @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    // GET request for searching API
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String, String>
     ): Response<FoodRecipe>
 }
