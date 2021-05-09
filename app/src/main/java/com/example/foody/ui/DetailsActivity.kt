0529f1064396ee2e595ceeca932f92595e983e12
JAbozarOid <araghibdoust@hotmail.com>
2021-05-09 13:09:41 +0430

    create_pager_adapter

--- app/src/main/java/com/example/foody/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/foody/ui/DetailsActivity.kt
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.example.foody.R
+import com.example.foody.adapters.PagerAdapter
+import com.example.foody.ui.fragments.ingredinets.IngredientFragment
+import com.example.foody.ui.fragments.instructions.InstructionsFragment
+import com.example.foody.ui.fragments.overview.OverviewFragment
 import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -15,6 +24,37 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // initialize pager adapter here and add fragments that we want to show in view pager, so we add fragments in specific order
+        // before initialize pager adapter we need to create bundle object, so we need to get the values or the data from our safe, which we have passed from our recipes fragment to
+        // our detailed activity
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(IngredientFragment())
+        fragments.add(InstructionsFragment())
+
+        // add titles for fragments
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        // initialize pager adapter
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+
+        // set the pager adapter to view pager in details activity
+        viewPager.adapter = pagerAdapter
+
+        // set each fragments to each related fragments
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     // when user click on the back button on the toolbar
