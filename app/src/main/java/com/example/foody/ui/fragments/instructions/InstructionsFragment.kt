dca5102405a26eafeb822acb4b4340de89bdc6d8
JAbozarOid <araghibdoust@hotmail.com>
2021-05-11 11:40:19 +0430

    instruction_fragment

--- app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.foody.R
+import com.example.foody.models.Result
+import com.example.foody.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
 
@@ -20,7 +24,19 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        // get arguments from bundle which bundle hold the result which means recipe
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        view.instruction_webView.webViewClient = object : WebViewClient() {}
+
+        // because myBundle.sourceUrl is nullable and the method "loadUrl" accept only String we must first use this operator !!.
+        val websiteUrl: String = myBundle!!.sourceUrl
+        view.instruction_webView.loadUrl(websiteUrl)
+
+        return view
     }
 
 
